62ed6792bc8a575e442aa6808dc59d0aa4cf4ede
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2024-10-29 20:32:01 -0600

    update readme.md

--- setup.py
+++ setup.py
@@ -12,28 +12,53 @@ class PostInstallCommand(install):
         # Definir la ruta para el archivo JSON
         json_file_path = os.path.expanduser('~/.rayoptix/simulation_folders.json')
         
-        # Crear el directorio si no existe
-        os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
-        print(json_file_path)
-        # Si el archivo no existe, crearlo con un diccionario vacío
-        if not os.path.exists(json_file_path):
-            with open(json_file_path, 'w') as json_file:
-                json.dump({}, json_file, indent=4)
-            print(f"Created JSON file at {json_file_path}")
-        else:
-            print(f"JSON file already exists at {json_file_path}")
+        try:
+            # Crear el directorio si no existe
+            os.makedirs(os.path.dirname(json_file_path), exist_ok=True)    
+            # Si el archivo no existe, crearlo con un diccionario vacío
+            if not os.path.exists(json_file_path):
+                with open(json_file_path, 'w') as json_file:
+                    json.dump({}, json_file, indent=4)
+                print(f"Created JSON file at {json_file_path}")
+            else:
+                print(f"JSON file already exists at {json_file_path}")
+        
+        except Exception as e:
+            print(f"Error creating JSON file: {e}")
 
 setup(
-    name='rayoptix',
-    version='0.1.0',
+    name='rayoptix',  # El nombre de tu paquete en PyPI
+    version='1.0.1',  # Asegúrate de actualizar la versión con cada release
     packages=find_packages(),
-    install_requires=[],  # Añade tus dependencias aquí
+    install_requires=[
+        "bifacial_radiance",
+        "pandas",
+        "matplotlib",
+        "sutil",
+        "GPUtil",
+        "seaborn",
+        "psutil"
+    ],
     entry_points={
         'console_scripts': [
-            'rayoptix=rayoptix.main:cli',  # Cambia según tu función principal
+            'rayoptix=rayoptix.main:cli',
         ],
     },
     cmdclass={
-        'install': PostInstallCommand,  # Vincular el comando de instalación con PostInstallCommand
+        'install': PostInstallCommand,
     },
+    author="Aldo Cambronero Ureña",  # Tu nombre o el de tu organización
+    author_email="cambroneroaldo03@gmail.com",
+    description="Rayoptix is a terminal-based Python library for creating and evaluating simulations of bifacial photovoltaic systems using ray tracing. It allows users to define modules, configure experimental conditions, and analyze energy performance in complex solar setups.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Daval03/CE-114-rayoptix",  # URL del repositorio del proyecto
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Ajusta según tu licencia
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',  # Versión mínima de Python requerida
+    include_package_data=True,  # Incluye otros archivos especificados en MANIFEST.in
 )
